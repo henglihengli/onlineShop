e65d48ec7f8c1df9e31f4e109d8ad2bc464f7404
henglihengli <lsliheng@gmail.com>
2020-08-01 16:25:36 -0700

    onlineShop update Hibernate

--- src/main/java/onlineShop/Application.java
+++ src/main/java/onlineShop/Application.java
@@ -3,14 +3,17 @@ package onlineShop;
 import java.math.BigDecimal;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+// import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import onlineShop.log.PaymentAction;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("payment.xml");
+        // ApplicationContext context = new ClassPathXmlApplicationContext("payment.xml");
+        ApplicationContext context = 
+                new AnnotationConfigApplicationContext(ApplicationConfig.class);
         PaymentAction paymentAction = (PaymentAction) context.getBean("paymentAction");
-        paymentAction.pay(new BigDecimal(2)); 
+        paymentAction.pay(new BigDecimal(2));
      }
 }
